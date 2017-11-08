eb546433a5c76e0d0327bf286de724a0c2c9dd23
1ogon1 <konovalenkoruslan@gmail.com>
2017-11-08 11:09:55 +0200

    print first line

--- main.c
+++ main.c
@@ -1,32 +1,56 @@
 #include "get_next_line.h"
 
-static void	ft_write_line(char **str, char **line, int i)
+static int  ft_str_len_tonl(char *str)
 {
-	while (*str[i] != '\n')
-	{
-		*line[i] = *str[i];
-		i++;
-	}
+    int     len;
+
+    len = 0;
+    if (!str)
+        return (0);
+    while (str[len])
+    {
+        if (str[len] == '\n' || str[len] == '\0')
+            break ;
+        len++;
+    }
+    return (len);
 }
 
-static void	ft_first_line(const int fd, char **line, char **str)
+static char	*ft_write_line(char *str, int i)
+{
+    char    *res;
+
+    res = (char *)malloc(sizeof(char) * ft_str_len_tonl(str) + 1);
+	while (str[i] != '\n')
+	{
+        res[i] = str[i];
+		i++;
+	}
+    res[i] = '\0';
+    return (res);
+}
+
+static char *ft_first_line(const int fd, char **line, char *str)
 {
 	int 	ret;
 	char	buff[BUFF_SIZE + 1];
 
 	if (fd < 0)
-		return ;
+		return (0);
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[ret] = '\0';
-		if (!*str)
-			*str = ft_strdup(buff);
+		if (!str)
+			str = ft_strdup(buff);
 		else
-			*str = ft_strjoin(*str, buff);
-		if (ft_strrchr(*str, '\n'))
-			ft_write_line(str, line, 0);
-		free(buff);
+			str = ft_strjoin(str, buff);
+		if (ft_strrchr(str, '\n'))
+        {
+			*line = ft_write_line(str, 0);
+            break ;
+        }
 	}
+    return (str);
 }
 
 int 	get_next_line(const int fd, char **line)
@@ -36,7 +60,7 @@ int 	get_next_line(const int fd, char **line)
 	int			res;
 
 	if (!str)
-		ft_first_line(fd, line, &str);
+		str = ft_first_line(fd, line, str);
 	if (fd < 0)
 		return (-1);
 	if (!str)
